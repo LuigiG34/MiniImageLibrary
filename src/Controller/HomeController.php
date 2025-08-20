7ecb586cc3b7706e17a762109c590bdef96b08ab
Luigi Gandemer <luigigandemer6@gmail.com>
2025-08-20 19:16:31 +0200

    Auth system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,14 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController
+final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function __invoke(): Response
     {
+        dd($this->getUser());
         return new Response('<h1>It works 🎉</h1>');
     }
 }
