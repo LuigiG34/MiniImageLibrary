2ab13ca26412cbab0977cc44e73429b9e6adb192
Luigi Gandemer <luigigandemer6@gmail.com>
2025-08-21 16:00:50 +0200

    Return image by ID

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -30,5 +30,23 @@ final class ImageController extends AbstractController
         ]);
     }
 
-    
+    #[Route('/file/{id}', name: 'file_raw', requirements: ['id' => '[a-f0-9]{24}'], methods: ['GET'])]
+    public function fileById(string $id, DocumentManager $dm): Response
+    {
+        // Get repo
+        $repo = $dm->getRepository(Image::class);
+
+        // Find Document by id
+        $doc = $repo->find($id);
+        if (!$doc) {
+            throw $this->createNotFoundException();
+        }
+
+        $mime = $doc->getMime() ?: 'application/octet-stream';
+
+        // Return the Image
+        return new StreamedResponse(function () use ($repo, $id) {
+            $repo->downloadToStream($id, fopen('php://output', 'wb'));
+        }, 200, ['Content-Type' => $mime]);
+    }
 }
